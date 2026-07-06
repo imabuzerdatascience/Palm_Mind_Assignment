01c98894d98f15e94814c8babf222a546da557c5
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-06 17:24:02 +0545

    create api for uploady pdf and txt

--- app/api/upload.py
+++ app/api/upload.py
@@ -23,7 +23,35 @@ class UploadResponse(BaseModel):
 
 @router.post("/upload") 
 async def upload_documnet (
-    file : UploadFile = File(...)
+    file : UploadFile = File(...)): 
+
+    if not file.filename.lower().endswitch((".pdf" , ".txt")) :
+        raise HTTPException(status_code=400 , detail="only PDF and TXT are aloowed ")
+    
+    try : 
+        # save file 
+        file_path = os.path.join(Uploads_dir , file.filename)
+
+        with open(file_path, "wb") as buffer : 
+            shutil.copyfileobj(file.file, buffer) 
+
+        file_size = os.path.getsize(file_path)
+
+        return UploadResponse(
+            message= "File uploaded successfully" ,
+            filename = file.filename , 
+            file_size = file_size 
+        )
+    except Exception as e : 
+        raise HTTPException(status_code = 500 , detail = f"upload failed:{str(e)}")
 
 
-) :
+  
+
+
+
+ 
+
+
+
+  
