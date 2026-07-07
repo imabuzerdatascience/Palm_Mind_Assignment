eda3829de65fe30b1b4157ca301e02655b2c1647
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-07 15:59:58 +0545

    create vector_service

--- app/services/Vector_databse.py
+++ app/services/Vector_databse.py
@@ -6,3 +6,47 @@ import uuid
 from datetime import datetime
 from typing import List
 
+class vector_service :
+    def __init__(self):
+        self.client = QdrantClient(host="localhost" , port=6333)
+        self.collection_name = "documnets" 
+
+        # using HugginfaceEmbedding 
+        self.embedding = HuggingFaceEmbeddings(
+            model_name = "sentence-transformers/all-MiniLM-L6-v2", 
+
+        )
+    def create_collection(self): 
+        try :
+            self.client.get_collection(self.collection_name)
+        except :
+            self.client.create_collection (
+                collection_name = self.collection_name  ,
+                vectors_config = VectorParams(size=384 ,distance = Distance.Cosine) 
+            )
+    
+
+# store chunks 
+    def Store_chunks(self , chunks : List , filename:str) -> str:
+        self.create_collection()
+
+        document_id =str(uuid.uuid4())
+        embbed_chunks = self.embedding.embed_documnets([chunk.page_content for chunk in chunks]) 
+
+        points = [ 
+            PointStruct (
+                id = str(uuid.uuid4()) ,
+                vector = vector , 
+                payload = {
+                    "document_id": document_id,
+                    "filename": filename,
+                    "chunk_index": i,
+                    "text": chunks[i].page_content,
+                    "upload_time": datetime.now().isoformat()
+                }
+            )
+            for i ,  vector in enumerate(embbed_chunks) 
+
+        ]
+        
+        self.client.upsert(collection_name=self.collection_name , point = points)
