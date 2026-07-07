d8364aee2ef37acd2529b2b6a7ab627e1ce72cd9
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-07 17:28:13 +0545

    CREATE REDIS SERVICE FOR CHAT

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -9,13 +9,14 @@ load_dotenv()
 
 class Redis_Service() :
     def __init__(self):
-      self.redis.client = redis.Redis(
-         host = "localhost" 
+      self.redis_client = redis.Redis(
+         host = "localhost" ,
          port = 6379 , 
-         decode_response = True
+         decode_responses=True , 
+         socket_connect_timeout = 5 
       )
 
-      self.session_prefix = "chat session"
+      self.session_prefix = "chat"
    
 
 # save message 
@@ -28,3 +29,18 @@ class Redis_Service() :
           "content " : content , 
           "timestamp" : str(os.times()[4])
        }
+       
+       self .redis_client.rpush(key, json.dumps(message))
+       self.redis_client.ltrim(key, -20 , -1)
+
+    def get_history(self , session_id:str)-> List[Dict] :
+       key = f"{self.session_prefix}{session_id}"
+       messages = self.redis_client.lrange(key , 0 , -1)
+       return [json.loads(msg)for msg in messages] 
+    
+    def clear_history(self , session_id : str) :
+       key = f"{self.session_prefix}{session_id}"
+       self.redis_client.delete(key)
+
+
+redis_service = Redis_Service()
